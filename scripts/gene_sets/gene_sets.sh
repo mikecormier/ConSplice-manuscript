3516df74ef902fbc500ad5085b011663f18115ed
Michael Cormier <cormiermichaelj@gmail.com>
2021-10-07 17:24:33 -0600

    Gene sets script

---
+++ scripts/gene_sets/gene_sets.sh
@@ -0,0 +1,36 @@
+
+# Data donwloaded into the data directory of this repo
+
+
+## HI, AD, AR, OR Gene lists
+
+### Haploinsufficient Genes hosted on the MaCarthur lab gene list GitHub repo
+wget -O ../data/HI.genes.tsv https://raw.githubusercontent.com/macarthur-lab/gene_lists/master/lists/clingen_level3_genes_2018_09_13.tsv
+
+### Autosomal Dominant Genes hosted on the MaCarthur lab gene list GitHub repo
+wget -O ../data/AD.genes.tsv https://raw.githubusercontent.com/macarthur-lab/gene_lists/master/lists/berg_ad.tsv
+
+### Autosomal Recessive Genes hosted on the MaCarthur lab gene list GitHub repo
+wget -O ../data/AR.genes.tsv https://raw.githubusercontent.com/macarthur-lab/gene_lists/master/lists/all_ar.tsv
+
+### Olfactory Receptor Genes hosted on the MaCarthur lab gene list GitHub repo
+wget -O ../data/OR.genes.tsv https://raw.githubusercontent.com/macarthur-lab/gene_lists/master/lists/olfactory_receptors.tsv
+
+
+## Homozygous Tolerant genes hosted on the MaCarthur lab gene list GitHub repo
+wget -O ../data/Hom.LoF_tolerant.genes.tsv https://raw.githubusercontent.com/macarthur-lab/gene_lists/master/lists/homozygous_lof_tolerant_twohit.tsv
+
+
+## CRISPR Essential and Non-Essential Gene lists
+
+### CRISPR Essential Genes hosted on the MaCarthur lab gene list GitHub repo
+wget -O ../data/CRISPR.Essential.genes.tsv https://raw.githubusercontent.com/macarthur-lab/gene_lists/master/lists/CEGv2_subset_universe.tsv
+
+### CRISPR Non-Essential Genes hosted on the MaCarthur lab gene list GitHub repo
+wget -O ../data/CRISPR.Nonessential.genes.tsv https://raw.githubusercontent.com/macarthur-lab/gene_lists/master/lists/NEGv1_subset_universe.tsv
+
+
+## Developmental Delay and Intelectual Disability Gene Lists
+
+### DD/ID genes from the Deciphering Developmental Delay studies hosted on the Gene2Phenotype website
+wget -O ../data/DD.ID.genes.csv.gz https://www.ebi.ac.uk/gene2phenotype/downloads/DDG2P.csv.gz
